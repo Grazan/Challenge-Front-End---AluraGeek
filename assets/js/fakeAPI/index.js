a29b3179c7c392c8a6cbe9b3eb2157e3ba37daa3
Samuel Grazan <90648893+Grazan@users.noreply.github.com>
2023-07-27 00:14:01 -0300

    Main

---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,214 @@
+
+export const lista = [{
+  titulo: "Star Wars",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Funko Pop Star Wars - Darth Vader 456 Special Art",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_star_wars_darth_vader_456_special_art_8019_1_c42e2ed8a7e6f87bc464c97c531f1093.jpg",
+      preco: 149.90,
+      descricao: "O Funko Pop Star Wars Darth Vader 456 Special Art é um boneco colecionável incrível que vai agradar tanto os fãs de Star Wars quanto os colecionadores de Funko Pop! Este boneco apresenta a figura icônica de Darth Vader em uma versão especial de arte, tornando-o um item único e exclusivo.",
+      alt: "Darth Vader"
+    }
+  },
+  {
+    infos: {
+      id: 2,
+      nome: "Funko Pop Star Wars - Stormtrooper 455 Special Art",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_star_wars_stormtrooper_455_special_art_8021_1_84a41f98d440cfc23024c49dcfa9978e.jpg",
+      preco: 149.90,
+      descricao: "Funko Pop Stormtrooper Retro Series Star Wars Original - 455",
+      alt: "Stormtrooper"
+    }
+  },
+  {
+    infos: {
+      id: 3,
+      nome: "Funko Pop Star Wars C-3PO 454",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_star_wars_c_3po_454_8025_1_30986cca010eeb68c0f6522ee024e53e.jpg",
+      preco: 149.90,
+      descricao: "Funko Pop! Star Wars: Retro Series - C-3PO #454 - Produto Oficial Funko",
+      alt: "C-3PO"
+    }   
+
+  },
+  {
+    infos: {
+      id: 4,
+      nome: "Funko Pop Star Wars Baby Yoda 465 Cookies",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_star_wars_baby_yoda_cookies_7997_1_26ab7aa47c74081a0f7e2ad8521b7e29.jpg",
+      preco: 149.90,
+      descricao: "Grogu ou Baby Yoda comumente conhecido, é um personagem da série de televisão original The Mandalorian. Ele é um membro infantil da mesma espécie alienígena sem nome dos personagens de Star Wars, Yoda e Yaddle, com quem compartilha uma forte habilidade na força.",
+      alt: "Baby Yoda"
+    }
+  },
+{
+  infos: {
+    id: 5,
+    nome: "Funko Pop Star Wars Baby Yoda 465 Cookies",
+    img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_luke_skywalker_453_8023_1_ca791ae0ed231313e9cd8bb099a3988b.jpg",
+    preco: 149.90,
+    descricao: "O Funko Pop Star Wars Retro Series Luke Skywalker 453 é um boneco colecionável incrível que todo fã de Star Wars e de Funko Pop deve ter em sua coleção. Este boneco apresenta Luke Skywalker em sua roupa icônica da trilogia original de Star Wars, com uma pose clássica e uma expressão marcante em seu rosto.",
+    alt: "Luke Skywalker"
+  }
+},
+{
+  infos: {
+    id: 6,
+    nome: "Funko Pop Star Wars Kylo e Rey -2Peças",
+    img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_star_wars_kylo_e_rey_2pecas_7541_1_a4edcdeb03e76606c18dbf229dbf6199.jpg",
+    preco: 149.90,
+    descricao: "Se você é um fã de Star Wars, o conjunto Funko Pop! Star Wars Kylo & Rey 2 Peças é um item obrigatório para a sua coleção. Este conjunto apresenta dois bonecos colecionáveis em vinil, com cerca de 10 cm de altura cada um, que representam os personagens Kylo Ren e Rey.",
+    alt: "Kylo Ren e Rey"
+  }
+},
+{
+  infos: {
+    id: 7,
+    nome: "Funko POP Boba Fett Star Wars: The Mandalorian",
+    img: "https://images.tcdn.com.br/img/img_prod/723087/funko_pop_boba_fett_star_wars_the_mandalorian_462_8197_3_820878ffbf33cdc62191515477f0b595.png",
+    preco: 149.90,
+    descricao: "Este boneco colecionável é uma adição incrível à sua coleção de Star Wars. Com seus olhos grandes e expressivos, este Funko POP de Boba Fett é muito mais do que um simples boneco - ele é uma representação detalhada e fiel ao personagem amado pelos fãs.",
+    alt: "Boba Fett"
+  }
+},
+  ]
+},
+{
+  titulo: "Console",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Console Sony Playstation 5 ",
+      img: "https://images.kabum.com.br/produtos/fotos/238671/console-sony-playstation-5_1634132556_gg.jpg",
+      preco: 4256.9,
+      descricao:"Jogar no PS5 Não Tem Limites. Desfrute do carregamento do seu PS5, extremamente rápido com o SSD de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para PlayStation.",
+      alt:"PS5" 
+    }
+   },
+  {
+    infos: {
+      id: 2,
+      nome: "Console Xbox Series X",
+      img: "https://m.media-amazon.com/images/I/51A41nLe5IL._AC_SL1200_.jpg",
+      preco: 4349.9,
+      descricao:"Apresentamos o Xbox Series x, nosso console mais rápido e poderoso de todos os tempos. Jogue milhares de títulos de quatro gerações de consoles- todos os jogos têm melhor aparência e são melhor executados no Xbox Series x.",
+      alt:"XBOX SX" 
+    }
+  },
+    {
+    infos: {
+      id: 3,
+      nome: "Nitendo Switch",
+      img: "https://assets.nintendo.com/image/upload/f_auto/q_auto/c_fill,w_800/ncom/en_US/switch/system/three-modes-in-one",
+      preco: 2175.3,
+      descricao:"O Nintendo Switch foi desenvolvido para fazer parte da sua vida, transformando-se de um console doméstico em um console portátil num piscar de olhos.",
+      alt:"Nitendo Switch" 
+    }
+  },
+    {
+    infos: {
+      id: 4,
+      nome: "Steam Deck",
+      img: "https://d5gag3xtge2og.cloudfront.net/producao/33283282/G/steam_2.jpg",
+      preco: 3395.0,
+      descricao:"Steam Deck é um híbrido de console de jogos eletrônicos e tablet desenvolvido pela Valve Corporation",
+      alt:"Steam Deck" 
+    }
+  },
+    {
+    infos: {
+      id: 5,
+      nome: "Console Atari Flashback X TecToy com 110 Jogos e 2 Joysticks",
+      img: "https://casaevideodigital.vtexassets.com/arquivos/ids/333011/Console-Atari-2600-TecToy-1721879b.jpg?v=637656114088170000",
+      preco: 699.9,
+      descricao:"O seu primeiro videogame está de volta! O Console Atari Flashback X está de volta, para matar a saudade de quem nunca se esqueceu do clássico dos anos 80. O Atari Flashback X possui 110 jogos de video game integrados. Esses jogos incluem sucessos como Adventure, Asteroids®, Centipede®, Chopper Command ™, Frogger, Missile Command®, Pitfall! ™, Space Invaders ™, Yars ‘Revenge® e muito mais! O Atari Flashback X possui recursos modernos e impressionantes!",
+      alt:"Atari Flashback" 
+    }
+  },
+    {
+    infos: {
+      id: 6,
+      nome: "Fliperama Portátil 100% Digital Controle Arcade",
+      img: "https://cdn.awsli.com.br/600x700/2004/2004081/produto/113274650/995fe2075f.jpg",
+      preco: 1300.0,
+      descricao:"Nosso novo modelo de Fliperama Portátil Top Games conta com varias melhorias como novas estampas personalizadas e exclusivas, tampo em acrílico cristal, painel traseiro plug-in play, botão de power com filtro para proteção, pés de acrílico emborrachado.",
+      alt:"Fliperama" 
+    }
+  },
+    {
+    infos: {
+      id: 7,
+      nome: "Jogos digitais",
+      img: "https://cf.shopee.com.br/file/fba286dd46ff90e7495e86f8d6845721",
+    preco: 30.0,
+      descricao:"Todos os nossos jogos são em midia digital! ",
+        alt:"Jogos digitais" 
+    }
+  },
+  ]
+},
+{
+  titulo: "Diversos",
+  produtos: [{
+    infos: {
+      id: 1,
+      nome: "Luminaria Mike Wazowski - Disney",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/luminaria_mike_wazowski_disney_8659_1_d2317072b2919b494301c8127d9d2710.png",
+      preco: 139.9,
+      descricao:"A Luminária Disney Mike é equipada com luzes LED de baixo consumo de energia, que proporcionam uma iluminação suave e agradável. Ela é capaz de criar um clima aconchegante em qualquer ambiente, seja no quarto, sala de estar, escritório ou qualquer outro lugar que você desejar.",
+      alt:"Luminaria" 
+    }
+   },
+  {
+    infos: {
+      id: 2,
+      nome: "Tapete DrPepper",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/tapete_drpepper_paiaco_nao_ha_razao_para_voce_estar_aqui_8611_4_53710e2a34c229e84d3a8a6210784b9a.png",
+      preco: 39.9,
+      descricao:"Tapete DrPepper",
+      alt:"Tapete DrPepper" 
+    }
+  },
+    {
+    infos: {
+      id: 3,
+      nome: "Bandeirinhas de Natal Poke",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/bandeirinhas_de_natal_poke_7823_1_dc0866a86b544ddb2b6c3c583a100a47.jpg",
+      preco: 19.20,
+      descricao:"Bandeirinhas de Natal Poke",
+      alt:"Bandeirinhas de Natal Poke" 
+    }
+  },
+    {
+    infos: {
+      id: 4,
+      nome: "Cofre Batman - Cerâmica",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/cofre_ceramica_wb_dc_or_batman_bat_preto_amarelo_7169_1_20201214140539.jpg",
+      preco: 64.90,
+      descricao:"Cofre Batman - Cerâmica",
+      alt:"Cofre Batman - Cerâmica" 
+    }
+  },
+    {
+    infos: {
+      id: 5,
+      nome: "Caneca Espresso Patronum",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/180_caneca_3d_espresso_patronum_510ml_8631_1_1966bdc92af35f499cb700e8ee829b0c.png",
+      preco: 47.9,
+      descricao:"Caneca Espresso Patronum",
+      alt:"Caneca Espresso Patronum" 
+    }
+  },
+    {
+    infos: {
+      id: 6,
+      nome: "Almofada formato Plataforma 9 3/4 - Harry Potter",
+      img: "https://images.tcdn.com.br/img/img_prod/723087/almofada_formato_plataforma_9_3_4_harry_potter_7659_1_4c01c5f4cb24ca8076bf512add2ead85.jpg",
+      preco: 54.9,
+      descricao:"Almofada formato Plataforma 9 3/4 - Harry Potter",
+      alt:"Almofada 3/4" 
+    }
+  },
+  ]
+}]
